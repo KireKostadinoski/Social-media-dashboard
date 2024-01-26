9473ebaa205b25dcc9529414d4bf1e0ed8b8dfd0
Kire Kostadinoski <k.kostadinoski@outlook.com>
2024-01-26 15:46:15 +0100

    upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let toogleBtn = document.getElementById('mode');
 
 toogleBtn.onclick = function(){
-    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
 }
